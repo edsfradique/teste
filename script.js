25bb2dd17fc5a04037702d62fa944224dff41f82
Eduardo Fradique <edsfradique@outlook.com.br>
2025-03-15 16:59:58 -0300

    commit eduardo

---
+++ script.js
@@ -0,0 +1,4 @@
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const button = document.getElementById("button");
+const h1 = document.getElementById("h1");
